dc9da280d95048f5d15a780e3d5e453af3da3ae6
Ersin Aydın <ersinaydinworks@gmail.com>
2023-03-21 23:46:02 +0300

    add searchHeader  and avios

---
+++ src/SearchHeader.js
@@ -0,0 +1,9 @@
+function SearchHeader() {
+    return <div className="searchDiv">
+        <form>
+            <label>Ne ariyorsunuz?</label>
+            <input />
+        </form>
+    </div>;
+}
+export default SearchHeader;
